43537b93e60b0336d85a4609a5b94f72fe0b2b3f
unknown <dotnetcloudmc122@iiht.tech>
2020-11-03 15:08:47 +0530

    Update to Code

---
+++ ServiceAccount/Request.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceAccount
+{
+    class Request
+    {
+        public string name { get; set; }
+        public bool creditonhold { get; set; }
+        public string address1_latitude { get; set; }
+        public string description { get; set; }
+        public string revenue { get; set; }
+        public int accountcategorycode { get; set; }
+
+    }
+}
